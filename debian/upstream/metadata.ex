0b7f74952e3b056a6630a65e6573511c5ce03146
Erich Eickmeyer <erich@ericheickmeyer.com>
2022-09-08 10:13:27 -0700

    Add debian directory

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/plasma-distro-release-nofifier/issues
+# Bug-Submit: https://github.com/<user>/plasma-distro-release-nofifier/issues/new
+# Changelog: https://github.com/<user>/plasma-distro-release-nofifier/blob/master/CHANGES
+# Documentation: https://github.com/<user>/plasma-distro-release-nofifier/wiki
+# Repository-Browse: https://github.com/<user>/plasma-distro-release-nofifier
+# Repository: https://github.com/<user>/plasma-distro-release-nofifier.git
